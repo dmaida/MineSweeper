86958e7ca7c04193ab20b582d06b53ea4e054c44
Daniel Esteban Maida <daniel.maida@wsu.edu>
2016-04-11 11:18:44 -0700

    My code is able to set mines randomly and able to calculate number of adjacent mines for every cell. Now I need to work on exposing and automatically exposing cells once exposed() is called and the cell has 0 sarrounding mines.

--- Cell.java
+++ Cell.java
@@ -5,5 +5,5 @@ public class Cell {
     boolean hashMine; //Cell contain a mine?
     boolean exposed;  // Has the cell been exposed (may or may not have mine)?
     boolean marked;   // Cell marked has a mine (perhaps incorrectly)?
-    char numbSurroundingmines; //Number of mines in 8 adjacent cells;
+    int numbSurroundingmines; //Number of mines in 8 adjacent cells;
 }
--- MineField.java
+++ MineField.java
@@ -7,6 +7,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.util.Random;
+
+import static java.lang.Math.min;
 import static java.lang.Math.random;
 
 public class MineField extends Application {
@@ -18,8 +21,8 @@ public class MineField extends Application {
     private static final int X_TILES = W / TILE_SIZE;
     private static final int Y_TILES = H / TILE_SIZE;
 
-    private static final int numbMines = 20;
-    private static int numExposedCells;
+    private static final int numbMines = 20; // total number of mines
+    private static int numExposedCells = 0;  // total cell exposed
 
     private Cell[][] grid = new Cell[X_TILES][Y_TILES];
 
@@ -30,10 +33,12 @@ public class MineField extends Application {
         int n = w*h; //total cells left
         int m = numbMines; //Number of mines to set
 
+        int row, col;
+
         numExposedCells = 0;
 
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
+        for (row = 0; row < grid.length; row++) { // place mines
+            for ( col = 0; col < grid[0].length; col++) {
                 Cell cell = new Cell();
                 cell.exposed = cell.marked = cell.hashMine = false;
                 double p = (double) m / (double )n ; //probability of placing mine
@@ -43,29 +48,61 @@ public class MineField extends Application {
                     m--;
                 }
                 n--;
-                grid[i][j] = cell;
-               System.out.print(grid[i][j].hashMine + " ");
+                grid[row][col] = cell;
+                if (grid[row][col].hashMine) {
+                    System.out.print("X" + " ");
+                } else {
+                    System.out.print("*"+ " ");
+                }
             }
             System.out.print("\n");
         }
-
-        for (int i = 0; i < grid.length; i++) {//compute surrounding mine counts
-            for (int j = 0; j < grid[0].length; j++) {
+            System.out.print("\n");
+        for (row = 0; row < grid.length; row++) {//calculate surrounding mine counts
+            for (col = 0; col < grid[0].length; col++) {
+                int i, j, count = 0;
+                Cell cell = grid[row][col];
+                for (j = -1; j < +1; j++) {
+                    for (i = -1; i < +1; i++) {
+                        if (i == 0 && j == 0) continue;
+                        int rr = row + j, cc = col + i;
+                        if (rr < 0 || rr >= h || cc < 0 || cc >= w) continue;
+                        Cell neighbor = grid[rr][cc];
+                        if (neighbor.hashMine) {
+                            count++;
+                        }
+                    }
+                }
+                cell.numbSurroundingmines = count;
             }
         }
-
+        System.out.print("\n");
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                System.out.print(grid[i][j].numbSurroundingmines+ " ");
+            }
+            System.out.print("\n");
+        }
     }
 
-    public boolean mark(int column, int row ) {
+    public boolean mark(int column, int row ) { //mark a Cell
 
-        // mark a Cell
+        Cell cell = grid[row][column];
 
+        if (!cell.marked) { // if cell has NOT been marked, mark it
+            cell.marked = true;
+            return true;
+        }
+        else if (cell.marked) {// if cell is already  marked, unmark it
+            cell.marked = false;
+            return false;
+        }
         return false;
     }
 
-    public int expose (int column, int row ) {
-        /*
 
+    /*
+        expose(int column, int row)
         0 if a cell was safely exposed and no bombs are  adjacent to it. In this case,
         all adjacent cells with 0 adjacent bombs should also be exposed. These newly
         revealed neighbors can be revealed by calls to isExposed
@@ -74,23 +111,25 @@ public class MineField extends Application {
 
         -1 if a bomb was exposed at this time, game over.
 
-        At any point in time a call
+     */
 
-        */
 
+
+    public int expose (int column, int row ) {
         return 0;
     }
 
     public int isExposed(int column, int row) {
+
         return 0;
     }
 
-    public int unexposedCount() {
-        /*
+    /*
             At any point in time a call to unexposedCount() should return how many cells
             can be exposed without setting off a bomb
-        */
+     */
 
+    public int unexposedCount() {
         return 0;
     }
 
@@ -106,6 +145,9 @@ public class MineField extends Application {
 
         MineField mineField = new MineField();
         mineField.createMineField();
-        launch(args);
+
+        System.out.print("\n");
+        
+        //launch(args);
     }
 }
