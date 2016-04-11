4ed33c88e7015399fc7f86f1a73f9c3272fc5dea
dmaida <danielmaida2@gmail.com>
2016-04-10 22:53:57 -0700

    The code can randomly place minefields across the grid. Need to calculate number of adjacent bombs.

--- Controller.java
+++ Controller.java
@@ -2,4 +2,18 @@ package MineSweeper;
 
 public class Controller {
 
+    /*
+
+    The Controller should create a new Minefield Modle in response to the programs
+    initialization or in response to a press of the "start" button
+
+    Should call a mark method in the MineField object to toggle a cells marked status
+    in response to a left mouse click
+
+    Should call a expose method in the MineField object to expose a cell in response
+    to a right mouse click
+
+    Should update the View in response to mark/expose/start requests
+     */
+
 }
--- MineField.java
+++ MineField.java
@@ -4,42 +4,80 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
+
+import static java.lang.Math.random;
 
 public class MineField extends Application {
 
+    private static final int TILE_SIZE = 40;
+    private static final int W = 300;
+    private static final int H = 300;
+
+    private static final int X_TILES = W / TILE_SIZE;
+    private static final int Y_TILES = H / TILE_SIZE;
+
+    private static final int numbMines = 20;
+    private static int numExposedCells;
+
+    private Cell[][] grid = new Cell[X_TILES][Y_TILES];
 
 
-    public MineField createMineField(int column, int row) {
 
-        MineField mineField = new MineField();
+    public void createMineField() {
+        int w = X_TILES, h = Y_TILES;
+        int n = w*h; //total cells left
+        int m = numbMines; //Number of mines to set
 
-        Cell[][] cellMatrix = new Cell[row][column];
+        numExposedCells = 0;
 
-
-        Cell cell = new Cell();
-
-        cell.hashMine = false;
-        cell.exposed = false;
-        cell.marked = false;
-        cell.numbSurroundingmines = 0;
-
-        for (int i = 0; i < cellMatrix.length; i++) {
-            for (int j = 0; j < cellMatrix[0].length; j++) {
-                cellMatrix[i][j] = cell;
-
-                System.out.print(cellMatrix[i][j].exposed + " ");
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                Cell cell = new Cell();
+                cell.exposed = cell.marked = cell.hashMine = false;
+                double p = (double) m / (double )n ; //probability of placing mine
+                double g = random();
+                if (g < p) {
+                    cell.hashMine =true;
+                    m--;
+                }
+                n--;
+                grid[i][j] = cell;
+               System.out.print(grid[i][j].hashMine + " ");
             }
             System.out.print("\n");
         }
-        return mineField ;
+
+        for (int i = 0; i < grid.length; i++) {//compute surrounding mine counts
+            for (int j = 0; j < grid[0].length; j++) {
+            }
+        }
+
     }
 
     public boolean mark(int column, int row ) {
+
+        // mark a Cell
+
         return false;
     }
 
     public int expose (int column, int row ) {
+        /*
+
+        0 if a cell was safely exposed and no bombs are  adjacent to it. In this case,
+        all adjacent cells with 0 adjacent bombs should also be exposed. These newly
+        revealed neighbors can be revealed by calls to isExposed
+
+        1-8 if a cell was safely exposed and 1 or more bombs is adjacent to it
+
+        -1 if a bomb was exposed at this time, game over.
+
+        At any point in time a call
+
+        */
+
         return 0;
     }
 
@@ -48,6 +86,11 @@ public class MineField extends Application {
     }
 
     public int unexposedCount() {
+        /*
+            At any point in time a call to unexposedCount() should return how many cells
+            can be exposed without setting off a bomb
+        */
+
         return 0;
     }
 
@@ -59,11 +102,10 @@ public class MineField extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
 
         MineField mineField = new MineField();
-        mineField.createMineField(10, 5);
+        mineField.createMineField();
         launch(args);
     }
 }
